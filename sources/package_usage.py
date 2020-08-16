a402770706c0d00886520d18ca75b4338b19eabb
Dohyun Kim <00dh.kim@gmail.com>
2020-08-17 01:26:48 +0900

    Add argument show_idx in function selevis.tree()

--- sources/package_usage.py
+++ sources/package_usage.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time
-import selenium_visualization as sele_vis
+import selenium_visualization as selevis
 
 driver_path = 'chromedriver_83.exe'
 if __name__ == '__main__':
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     driver.get("http://lms.knu.ac.kr")
     mbox = driver.find_element_by_xpath("//div[@class='m-box2']")
 
-    sele_vis.tree(mbox, max_depth=2, show_text=True)
-    selected_node = sele_vis.select_node(6)
+    selevis.tree(mbox, max_depth=2, show_idx=True, show_text=True)
+    selected_node = selevis.select_node(6)
     print(repr(selected_node.text))
--- sources/selenium_visualization/func.py
+++ sources/selenium_visualization/func.py
@@ -5,14 +5,16 @@ element_list = []
 idx = 0
 
 
-def tree2(node, depth, max_depth, show_text):
+def tree2(node, depth, max_depth, show_idx, show_text):
     global element_list
     global idx
 
     if max_depth != -1 and depth > max_depth:
         return
 
-    print(' '*depth*2, '', node.tag_name, '[', idx, '] ', sep='', end='')
+    print(' '*depth*2, '', node.tag_name, sep='', end='')
+    if show_idx:
+        print('[',idx,']',sep='',end='')
     if show_text:
         print(repr(node.text[:20]), sep='', end='')
     print()
@@ -24,16 +26,16 @@ def tree2(node, depth, max_depth, show_text):
     if not sub_nodes:
         return
     for nd in sub_nodes:
-        tree2(nd, depth+1, max_depth, show_text)
+        tree2(nd, depth+1, max_depth, show_idx, show_text)
 
 
-def tree(node, max_depth=-1, show_text=False):
+def tree(node, max_depth=-1, show_idx=False, show_text=False):
     global element_list
     global idx
     element_list = []
     idx = 0
 
-    tree2(node, 0, max_depth, show_text)
+    tree2(node, 0, max_depth, show_idx, show_text)
 
 
 def select_node(idx):
